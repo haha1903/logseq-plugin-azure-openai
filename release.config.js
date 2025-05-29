86d226ad027ec760e041b783b2060af714c3ca30
Hai Chang <haichang@microsoft.com>
2025-05-29 22:56:55 +0800

    Update release config for Azure OpenAI deployment
    Adjusted the release configuration to change the zip file name to
    "logseq-plugin-azure-openai.zip" and updated files included in the zip,
    such as replacing readme.md with README.md and logo.png with icon.png.
    Additionally, disabled the success and fail comments, as well as release
    labels for GitHub.

--- release.config.js
+++ release.config.js
@@ -20,13 +20,16 @@ module.exports = {
       "@semantic-release/exec",
       {
         prepareCmd:
-          "mkdir build && zip -qq -r build/logseq-plugin-plugin-gpt3-openai.zip dist readme.md logo.png LICENSE package.json",
+          "mkdir build && zip -qq -r build/logseq-plugin-azure-openai.zip dist README.md icon.png LICENSE package.json",
       },
     ],
     [
       "@semantic-release/github",
       {
         assets: "build/*.zip",
+        successComment: false,
+        failComment: false,
+        releasedLabels: false,
       },
     ],
   ],
